0c79248ff168737af323f89b14240586ba939882
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-27 14:38:35 +0300

    Add shelter cat functionality

--- Cat-Shelter/src/controllers/catController.js
+++ Cat-Shelter/src/controllers/catController.js
@@ -38,4 +38,10 @@ router.get('/new-home/:catId', (req, res) => {
         .then(cat => res.render('catShelter', { cat }));
 });
 
+router.post('/new-home/:catId', (req, res) => {
+    const catId = req.params.catId;
+    catManager.deleteCat(catId);
+    res.redirect('/congrats');
+});
+
 module.exports = router;
