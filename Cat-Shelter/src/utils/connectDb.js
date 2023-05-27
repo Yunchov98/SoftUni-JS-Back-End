377c301937b03bf01ad807f4c7528ef4fbfd0688
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-27 13:05:59 +0300

    Update console text in db file

--- Cat-Shelter/src/utils/connectDb.js
+++ Cat-Shelter/src/utils/connectDb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDb() {
-    console.log('succefully');
+    console.log('succefully connected to the databse');
     return await mongoose.connect('mongodb://127.0.0.1:27017/catShelter');
 }
 
