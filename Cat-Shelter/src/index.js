828af4fb12d5c841f2e51668e4c01a2d9b41140f
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-27 15:07:35 +0300

    Add server and configurations

--- Cat-Shelter/src/index.js
+++ Cat-Shelter/src/index.js
@@ -1,7 +1,15 @@
-const catManager = require('./managers/catManager');
+const express = require('express');
 const connectDb = require('./utils/connectDb');
+const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
+const routes = require('./routes');
+
+const app = express();
+const PORT = 5000;
 
 connectDb();
+expressConfig(app);
+handlebarsConfig(app);
+app.use(routes);
 
-const cats = catManager.deleteCat('6470b7844987a584d0eb5b7b');
-
+app.listen(PORT, () => console.log('Server is listening on port 5000'));
