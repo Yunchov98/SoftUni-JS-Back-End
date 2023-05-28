4919d0efffc25dda8aa81bb20e19ed2f994267bf
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-28 17:11:33 +0300

    Add cube controller

---
+++ Cubicle/src/controllers/cubeController.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const cubeManager = require('../manager/cubeManager');
+
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+router.post('/create', (req, res) => {
+    const {
+        name,
+        description,
+        imageUrl,
+        difficultyLevel,
+    } = req.body;
+
+    cubeManager.createCube(name, description, imageUrl, difficultyLevel);
+
+    res.redirect('/');
+});
+
+module.exports = router;
