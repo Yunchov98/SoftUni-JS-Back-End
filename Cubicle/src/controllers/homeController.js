b89844ddaf804409b0de0a32478b4ea7fa9793d3
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-31 17:17:29 +0300

    Update home controller

--- Cubicle/src/controllers/homeController.js
+++ Cubicle/src/controllers/homeController.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 const cubeManager = require('../manager/cubeManager');
 
-router.get('/', (req, res) => {
-    cubeManager.getCubes()
-        .then(cubes => res.render('index', { cubes }));
+router.get('/', async (req, res) => {
+    const { search, from, to } = req.query;
+
+    const cubes = await cubeManager.getCubes(search, from, to);
+
+    res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
     res.render('about');
+});
+
+router.get('/404', (req, res) => {
+    res.render('404');
 });
 
 module.exports = router;
