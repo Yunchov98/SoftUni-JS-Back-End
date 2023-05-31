1876b7ba1bf9232350041e4c4f1db0750e3190f4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-31 17:11:39 +0300

    Remove console log from db

--- Cubicle/src/utils/databse.js
+++ Cubicle/src/utils/databse.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const DB_URL = require('./dbUrl');
 
 async function connectDb() {
-    console.log('Connection with DB - successfully');
     return await mongoose.connect(DB_URL);
 }
 
