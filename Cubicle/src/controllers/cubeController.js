5856b8d8837438a28671868e435442f23b82627a
Georgi Yunchov <georgiiunchov@gmail.com>
2023-05-31 17:16:36 +0300

    Add accessory manager to the cube controller

--- Cubicle/src/controllers/cubeController.js
+++ Cubicle/src/controllers/cubeController.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const cubeManager = require('../manager/cubeManager');
+const accessoryManager = require('../manager/accessoryManager');
 
 router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const {
         name,
         description,
@@ -13,16 +14,42 @@ router.post('/create', (req, res) => {
         difficultyLevel,
     } = req.body;
 
-    cubeManager.createCube(name, description, imageUrl, difficultyLevel);
+    await cubeManager.createCube({
+        name,
+        description,
+        imageUrl,
+        difficultyLevel: Number(difficultyLevel),
+    });
 
     res.redirect('/');
 });
 
-router.get('/details/:cubeId', (req, res) => {
-    cubeManager.getCubeById(req.params.cubeId)
-        .then(cube => {
-            res.render('details', { cube });
-        });
+router.get('/details/:cubeId', async (req, res) => {
+    const cube = await cubeManager.getOneWithAccessories(req.params.cubeId).lean();
+
+    if (!cube) {
+        res.redirect('/404');
+    }
+
+    res.render('details', { cube });
+});
+
+router.get('/attach-accessory/:cubeId', async (req, res) => {
+    const cube = await cubeManager.getCubeById(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getRest(cube.accessories).lean();
+
+    const hasAccessories = accessories.length > 0;
+
+    res.render('accessory/attach', { cube, accessories, hasAccessories });
+});
+
+router.post('/attach-accessory/:cubeId', async (req, res) => {
+    const { accessory: accessoryId } = req.body;
+    const cubeId = req.params.cubeId;
+
+    await cubeManager.attachAccessory(cubeId, accessoryId);
+
+    res.redirect(`/cubes/details/${cubeId}`);
 });
 
 module.exports = router;
