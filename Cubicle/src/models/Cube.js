f0a7ba42d8e0e6052a9042623c581f7589da45e9
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-08 12:36:40 +0300

    Make reference with user

--- Cubicle/src/models/Cube.js
+++ Cubicle/src/models/Cube.js
@@ -20,6 +20,10 @@ const cubeSchema = new mongoose.Schema({
     accessories: [{
         type: mongoose.Types.ObjectId,
         ref: 'Accessory',
+    }],
+    owner: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
     }]
 });
 
