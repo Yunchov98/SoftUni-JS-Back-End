e4c64b0ac8166619cee4b0a8168d537a1ca18c0b
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 11:22:27 +0300

    Add error handler middleware

---
+++ Cubicle/src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,7 @@
+const { getErrorMessage } = require('../utils//errorHelper');
+
+module.exports = (err, req, res, next) => {
+    const errorMessage = getErrorMessage(err);
+
+    res.render('/404', { errorMessage });
+};
