c0b3f10d4d9a215691e742efa845a3b0ff18c17e
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 21:39:36 +0300

    Add create game functionality

--- Gaming-Team/src/managers/gameManager.js
+++ Gaming-Team/src/managers/gameManager.js
@@ -1,9 +1,8 @@
 const Game = require('../models/Game');
 
 exports.getGames = () => Game.find();
-exports.createGame = (platfrom, name, imageUrl, price, genre, description) => {
-    Game.create(platfrom, name, imageUrl, price, genre, description);
-};
+exports.createGame = (gameData) => Game.create(gameData);
+
 
 exports.getGameById = (gameId) => Game.findById(gameId);
 exports.editGame = (gameId, platfrom, name, imageUrl, price, genre, description) => Game.findByIdAndUpdate(gameId, { platfrom, name, imageUrl, price, genre, description });
