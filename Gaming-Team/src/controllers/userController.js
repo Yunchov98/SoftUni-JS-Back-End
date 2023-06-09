620a81c58b84fffb1fc504b15aef0b972975d6f3
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 21:41:56 +0300

    Add functionality for post request in user contoller

--- Gaming-Team/src/controllers/userController.js
+++ Gaming-Team/src/controllers/userController.js
@@ -1,15 +1,47 @@
 const router = require('express').Router();
 
+const userManager = require('../managers/userManager');
+
 router.get('/register', (req, res) => {
     res.render('user/register');
 });
 
-// TODO: router.post('/register')...
+// TODO: catch error message and render it
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, confirmPassword } = req.body;
+
+    try {
+        await userManager.register({ username, email, password, confirmPassword });
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 router.get('/login', (req, res) => {
     res.render('user/login');
 });
 
-// TODO: router.post('/login')...
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    // TODO: catch error message and render it
+    try {
+        const token = await userManager.login(email, password);
+
+        res.cookie('auth', token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/logout', async (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
 
 module.exports = router;
