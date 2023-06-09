715195ca5ebb53d8d37633f5bccacd4bd5243061
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 21:38:37 +0300

    Add auth middleware

---
+++ Gaming-Team/src/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+const { SECRET } = require('../configs/utils');
+const jwtPromises = require('../lib/jwtPromises');
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if (token) {
+        const payload = await jwtPromises.verify(token, SECRET);
+
+        req.user = payload;
+        res.locals.user = payload;
+        res.locals.isAuthenticated = true;
+
+        next();
+    } else {
+        next();
+    }
+};
+
+exports.isAuth = (req, res, next) => {
+    if (!req.user) {
+        return res.redirect('/users/login');
+    }
+
+    next();
+};
