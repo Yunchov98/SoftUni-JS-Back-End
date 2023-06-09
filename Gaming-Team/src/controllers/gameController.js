742e97d1f07db5de8131e6f940aa257622582fe6
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 21:40:19 +0300

    Add functionality to post and get requests in game controller

--- Gaming-Team/src/controllers/gameController.js
+++ Gaming-Team/src/controllers/gameController.js
@@ -1,17 +1,53 @@
 const router = require('express').Router();
 
-router.get('/catalog', (req, res) => {
-    res.render('game/catalog');
+const gameManager = require('../managers/gameManager');
+
+router.get('/catalog', async (req, res) => {
+    const games = await gameManager.getGames().lean();
+
+    res.render('game/catalog', { games });
 });
 
 router.get('/create', (req, res) => {
     res.render('game/create');
 });
 
-// TODO: router.post('/create')
+router.post('/create', async (req, res) => {
+    const { platform, name, imageUrl, price, genre, description } = req.body;
 
-router.get('/details/:gameId', (req, res) => {
-    res.render('game/details');
+    try {
+        await gameManager.createGame({
+            platform,
+            name,
+            imageUrl,
+            price: Number(price),
+            genre,
+            description,
+            owner: req.user._id
+        });
+
+        res.redirect('/games/catalog');
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+router.get('/details/:gameId', async (req, res) => {
+    try {
+        const game = await gameManager.getGameById(req.params.gameId).lean();
+
+        if (!game) {
+            res.redirect('/404');
+        }
+
+        const isOwner = game.owner?.toString() === req.user?._id;
+
+        res.render('game/details', { game, isOwner });
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 router.get('/details/edit', (req, res) => {
