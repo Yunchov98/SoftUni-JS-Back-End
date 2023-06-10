e261c75aa11e5c0b18954cfcd679f365d10c6e1c
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-10 14:17:41 +0300

    Add validation for user and game models

--- Gaming-Team/src/models/Game.js
+++ Gaming-Team/src/models/Game.js
@@ -4,6 +4,7 @@ const gameSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name is required'],
+        minLength: [4, 'Name should be at least four characters long'],
     },
 
     imageUrl: {
@@ -19,11 +20,13 @@ const gameSchema = new mongoose.Schema({
     description: {
         type: String,
         required: [true, 'Description is required'],
+        minLength: [true, 'Description should be at least ten characters long'],
     },
 
     genre: {
         type: String,
         required: [true, 'Genre is required'],
+        minLength: [2, 'Genre should be at least two characters long'],
     },
 
     platform: {
@@ -49,6 +52,18 @@ gameSchema.path('platform').validate((platform) => {
 
     if (!availablePlatforms.includes(platform)) {
         throw new Error(`Only ${availablePlatforms.join(' ')} are available`);
+    }
+});
+
+gameSchema.path('imageUrl').validate((imageUrl) => {
+    if (!imageUrl.startsWith('http')) {
+        throw new Error('Image URL must starts with http:// ot https://');
+    }
+});
+
+gameSchema.path('price').validate((price) => {
+    if (Number(price) < 0) {
+        throw new Error('Price must be a possitive number');
     }
 });
 
--- Gaming-Team/src/models/User.js
+++ Gaming-Team/src/models/User.js
@@ -5,16 +5,19 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username is required'],
+        minLength: [5, 'Username should be at least five characters long'],
     },
 
     email: {
         type: String,
         required: [true, 'Email is required'],
+        minLength: [10, 'Username should be at least ten characters long'],
     },
 
     password: {
         type: String,
         required: [true, 'Password is required'],
+        minLength: [4, 'Username should be at least four characters long'],
     },
 });
 
