a8f3dae554d07f4e3d068c40754e6482b29aa9fc
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-10 12:46:42 +0300

    Add auth in the game controller

--- Gaming-Team/src/controllers/gameController.js
+++ Gaming-Team/src/controllers/gameController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const gameManager = require('../managers/gameManager');
+const { isAuth } = require('../middlewares/authMiddleware');
+const { getPlatformsViewData } = require('../utils/viewHelper');
 
 router.get('/catalog', async (req, res) => {
     const games = await gameManager.getGames().lean();
@@ -8,11 +10,11 @@ router.get('/catalog', async (req, res) => {
     res.render('game/catalog', { games });
 });
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('game/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const { platform, name, imageUrl, price, genre, description } = req.body;
 
     try {
@@ -36,21 +38,52 @@ router.post('/create', async (req, res) => {
 router.get('/details/:gameId', async (req, res) => {
     try {
         const game = await gameManager.getGameById(req.params.gameId).lean();
+        const user = req.user;
 
         if (!game) {
             res.redirect('/404');
         }
 
-        const isOwner = game.owner?.toString() === req.user?._id;
 
-        res.render('game/details', { game, isOwner });
+        const isOwner = game.owner?.toString() === req.user?._id;
+        const isBought = game.boughtBy?.toString() === req.user?._id && game.owner?.toString() !== game.boughtBy?.toString();
+
+        res.render('game/details', { game, isOwner, isBought, user });
     } catch (error) {
         console.log(error);
     }
 
 });
 
-router.get('/details/edit', (req, res) => {
-    res.render('game/edit');
+router.get('/edit/:gameId', isAuth, async (req, res) => {
+    const gameId = req.params.gameId;
+
+    try {
+        const game = await gameManager.getGameById(gameId).lean();
+
+        if (!game) {
+            res.redirect('/404');
+        }
+
+        const options = getPlatformsViewData(game.platform);
+
+        res.render('game/edit', { game, options });
+    } catch (error) {
+        console.log(error);
+    }
 });
+
+router.post('/edit/:gameId', isAuth, async (req, res) => {
+    const gameId = req.params.gameId;
+    const { platfrom, name, imageUrl, price, genre, description } = req.body;
+
+    try {
+        await gameManager.editGame(gameId, platfrom, name, imageUrl, price, genre, description);
+
+        res.redirect(`/games/details/${gameId}`);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 module.exports = router;
