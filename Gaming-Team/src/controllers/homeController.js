e544ec7f4d641f165c25d88aa22767737b88a710
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-10 14:19:05 +0300

    Add search page

--- Gaming-Team/src/controllers/homeController.js
+++ Gaming-Team/src/controllers/homeController.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 
+const gameManager = require('../managers/gameManager');
+
 router.get('/', (req, res) => {
     res.render('home');
 });
 
-router.get('/search', (req, res) => {
-    res.render('search');
+router.get('/search', async (req, res) => {
+    const { name, platform} = req.query;
+
+    try {
+        const games = await gameManager.getGames(name, platform);
+
+        res.render('search', { games, name, platform });
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 router.get('/404', (req, res) => {
