f0f6b40df9383b6ffe3424870a1a1b00589f86c9
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-11 16:04:15 +0300

    Add user controller

---
+++ Petstagram/src/controllers/userController.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+
+const userManager = require('../managers/userManager');
+
+router.get('/register', (req, res) => {
+    res.render('user/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, repeatPassword } = req.body;
+
+    try {
+        await userManager.register({ username, email, password, repeatPassword });
+
+        const token = await userManager.login(username, password);
+
+        res.cookie('auth', token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('user/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const token = await userManager.login(username, password);
+
+        res.cookie('auth', token, {httpOnly: true});
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+    res.redirect('/');
+});
+
+module.exports = router;
