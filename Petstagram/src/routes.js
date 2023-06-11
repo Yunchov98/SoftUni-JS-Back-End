c398abdbadade4da6202abba040b2cab940747f4
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-11 18:33:14 +0300

    Add photo controller to the routes

--- Petstagram/src/routes.js
+++ Petstagram/src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const userController = require('./controllers/userController');
+const photoController = require('./controllers/photoController');
 
 router.use(homeController);
 router.use('/users', userController);
+router.use('/pets', photoController);
 router.use('*', (req, res) => {
     res.render('404');
 });
