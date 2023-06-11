ab85f0e2216ed2f2898a57c1548a952254a8f875
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-11 16:04:59 +0300

    Add routes to the app file

--- Petstagram/src/index.js
+++ Petstagram/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const expressConfigs = require('./configs/expressConfigs');
 const handlebarsConfigs = require('./configs/handlebarsConfigs');
 const connectDb = require('./configs/database');
+const routes = require('./routes');
 
 const { PORT } = require('./configs/utils');
 
@@ -10,6 +11,8 @@ const app = express();
 
 expressConfigs(app);
 handlebarsConfigs(app);
+
+app.use(routes);
 
 connectDb()
     .then(console.log('Successfully connected to the database'))
