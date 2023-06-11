e0eedeac90763c5e6291ab5042d2c8887ba0500a
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-11 16:02:30 +0300

    Add photo model

---
+++ Petstagram/src/models/Photo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const photoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+
+    imageUrl: {
+        type: String,
+        required: [true, 'Image URL is required'],
+    },
+
+    age: {
+        type: Number,
+        required: [true, 'Age are required'],
+    },
+
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+    },
+
+    location: {
+        type: String,
+        required: [true, 'Location is required'],
+    },
+
+    commentList: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }],
+
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const Photo = mongoose.model('Photo', photoSchema);
+
+module.exports = Photo;
