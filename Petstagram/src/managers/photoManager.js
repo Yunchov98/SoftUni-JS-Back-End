487529b6699cabf32155d8bbfd67fb8254edf635
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-13 11:36:26 +0300

    Add get user phots functionality

--- Petstagram/src/managers/photoManager.js
+++ Petstagram/src/managers/photoManager.js
@@ -6,6 +6,7 @@ exports.createPhoto = (photoData) => Photo.create(photoData);
 exports.getPhotoById = (photoId) => Photo.findById(photoId).populate('owner');
 exports.deletePhoto = (photoId) => Photo.findByIdAndDelete(photoId);
 exports.updatePhoto = (photoId, name, age, description, location, imageUrl) => Photo.findByIdAndUpdate(photoId, { name, age, description, location, imageUrl });
+exports.getUserPhotos = (userId) => Photo.find({owner: userId});
 exports.addComment = (photoId, username, comment) => Photo.findByIdAndUpdate(photoId, {
     $push: {
         commentList: { id: username, comment }
