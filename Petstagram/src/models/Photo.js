3facea40df4ff9427680d0a0275c9add31a3ffa7
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-13 11:31:59 +0300

    Add validation to user schema

--- Petstagram/src/models/Photo.js
+++ Petstagram/src/models/Photo.js
@@ -4,6 +4,7 @@ const photoSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Name is required'],
+        minLength: [2, 'Name should be at least 2 characters long'],
     },
 
     imageUrl: {
@@ -19,11 +20,15 @@ const photoSchema = new mongoose.Schema({
     description: {
         type: String,
         required: [true, 'Description is required'],
+        minLength: [5, 'Description should be at least 5 characters long'],
+        maxLength: [50, 'Description cannot be longer than 50 characters'],
     },
 
     location: {
         type: String,
         required: [true, 'Location is required'],
+        minLength: [5, 'Location should be at least 5 charactes long'],
+        maxLength: [50, 'Location cannot be longer than 50 characters'],
     },
 
     commentList: [{
@@ -39,6 +44,18 @@ const photoSchema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         ref: 'User',
     },
+});
+
+photoSchema.path('imageUrl').validate(function (imageUrl) {
+    if (!imageUrl.startsWith('http')) {
+        throw new Error('Image URL should start with http:// or https://');
+    }
+});
+
+photoSchema.path('age').validate(function (age) {
+    if (age < 1 || age > 100) {
+        throw new Error('Age must be between 1 - 100');
+    }
 });
 
 const Photo = mongoose.model('Photo', photoSchema);
