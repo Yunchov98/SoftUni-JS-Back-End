9c3bc88d42a9901261937dcb79881f3918319a35
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-14 14:13:52 +0300

    Hashing the password before save

--- Crypto-Trade/src/models/User.js
+++ Crypto-Trade/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -23,6 +24,12 @@ userSchema.virtual('confirmPassword')
             throw new Error('Password missmatch');
         }
     });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 
