242c1ee244f3c256225ec35c3433f65d2ba2b5c8
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-14 13:08:42 +0300

    Add express configurations

--- Crypto-Trade/src/index.js
+++ Crypto-Trade/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const { PORT } = require('./configs/utils');
+const expressConfigs = require('./configs/expressConfigs');
 
 const app = express();
+
+expressConfigs(app);
 
 app.listen(PORT, () => console.log(`Sever is listening on port ${PORT}...`));
