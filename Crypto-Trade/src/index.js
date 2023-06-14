e579b2b4c43cdd5c847e97ec777b69e3eb427b5c
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-14 13:14:10 +0300

    Add express handlebars with configurations

--- Crypto-Trade/src/index.js
+++ Crypto-Trade/src/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const { PORT } = require('./configs/utils');
 const expressConfigs = require('./configs/expressConfigs');
+const handlebarsConfigs = require('./configs/handlebarsConfigs');
 
 const app = express();
 
 expressConfigs(app);
+handlebarsConfigs(app);
 
 app.listen(PORT, () => console.log(`Sever is listening on port ${PORT}...`));
