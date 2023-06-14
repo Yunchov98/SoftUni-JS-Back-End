2fa10aa3929de465f692529ffae3d998f075cbef
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-14 15:05:01 +0300

    Add login and register functionality

--- Crypto-Trade/src/controllers/userController.js
+++ Crypto-Trade/src/controllers/userController.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 const userManager = require('../managers/userManager');
+const { TOKEN_KEY } = require('../configs/utils');
+const { loginValidator, registerValidator } = require('../utils/validators');
 
 router.get('/register', (req, res) => {
     res.render('user/register');
@@ -10,16 +12,34 @@ router.post('/register', async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
 
     try {
+        registerValidator(username, email, password, confirmPassword);
+
         await userManager.register({ username, email, password, confirmPassword });
 
         res.redirect('/');
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 });
 
 router.get('/login', (req, res) => {
     res.render('user/login');
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        loginValidator(email, password);
+
+        const token = await userManager.login(email, password);
+
+        res.cookie(TOKEN_KEY, token, { httpOnly: true });
+
+        res.redirect('/');
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 module.exports = router;
--- Crypto-Trade/src/managers/userManager.js
+++ Crypto-Trade/src/managers/userManager.js
@@ -1,3 +1,29 @@
+const bcrypt = require('bcrypt');
+const jwtPromises = require('../lib/jwtPromises');
+
 const User = require('../models/User');
+const { SECRET } = require('../configs/utils');
 
 exports.register = (userData) => User.create(userData);
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Wrong username or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Wrong username or password');
+    }
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+    };
+
+    const token = await jwtPromises.sign(payload, SECRET);
+    return token;
+};
