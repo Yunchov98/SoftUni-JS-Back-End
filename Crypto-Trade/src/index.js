dda6863d4dfa3ba4748adc5fc1eb371ec1308ed6
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-14 13:31:59 +0300

    Create a home controller and add it to the routes

--- Crypto-Trade/src/index.js
+++ Crypto-Trade/src/index.js
@@ -3,10 +3,13 @@ const express = require('express');
 const { PORT } = require('./configs/utils');
 const expressConfigs = require('./configs/expressConfigs');
 const handlebarsConfigs = require('./configs/handlebarsConfigs');
+const routes = require('./routes');
 
 const app = express();
 
 expressConfigs(app);
 handlebarsConfigs(app);
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Sever is listening on port ${PORT}...`));
