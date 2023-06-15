cf92ef0948d24903d335befd1a01e94e94933326
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-15 16:53:07 +0300

    Add delete and update functionality

--- Crypto-Trade/src/managers/cryptoManager.js
+++ Crypto-Trade/src/managers/cryptoManager.js
@@ -1,7 +1,13 @@
 const Crypto = require('../models/Crypto');
 
 exports.createCrypto = (cryptoData) => Crypto.create(cryptoData);
+
 exports.getCryptos = () => Crypto.find();
+
 exports.getCryptoById = (cryptoId) => Crypto.findById(cryptoId);
+
 exports.buyCrypto = (cryptoId, userId) => Crypto.findByIdAndUpdate(cryptoId, { $push: { boughtBy: userId } });
+
 exports.deleteCrypto = (cryptoId) => Crypto.findByIdAndDelete(cryptoId);
+
+exports.updateCrypto = (cryptoId, cryptoData) => Crypto.findByIdAndUpdate(cryptoId, cryptoData);
