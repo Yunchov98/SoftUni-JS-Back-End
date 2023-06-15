af29e7658da9d7b8f3937cc7e1d64b6bf8eb6542
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-15 18:09:16 +0300

    Add search page

--- Crypto-Trade/src/controllers/homeController.js
+++ Crypto-Trade/src/controllers/homeController.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
+const cryptoManager = require('../managers/cryptoManager');
+
 router.get('/', (req, res) => {
     res.render('home');
+});
+
+router.get('/search', async (req, res) => {
+    const { search, select } = req.query;
+
+    try {
+        const cryptos = await cryptoManager.getCryptos(search, select);
+
+        res.render('search', {cryptos, search, select});
+    } catch (error) {
+        res.render('404');
+    }
+
 });
 
 module.exports = router;
--- Crypto-Trade/src/managers/cryptoManager.js
+++ Crypto-Trade/src/managers/cryptoManager.js
@@ -2,8 +2,6 @@ const Crypto = require('../models/Crypto');
 
 exports.createCrypto = (cryptoData) => Crypto.create(cryptoData);
 
-exports.getCryptos = () => Crypto.find();
-
 exports.getCryptoById = (cryptoId) => Crypto.findById(cryptoId);
 
 exports.buyCrypto = (cryptoId, userId) => Crypto.findByIdAndUpdate(cryptoId, { $push: { boughtBy: userId } });
@@ -11,3 +9,21 @@ exports.buyCrypto = (cryptoId, userId) => Crypto.findByIdAndUpdate(cryptoId, { $
 exports.deleteCrypto = (cryptoId) => Crypto.findByIdAndDelete(cryptoId);
 
 exports.updateCrypto = (cryptoId, cryptoData) => Crypto.findByIdAndUpdate(cryptoId, cryptoData);
+
+exports.getCryptos = async (search, select) => {
+    try {
+        let result = await Crypto.find().lean();
+
+        if (search) {
+            result = result.filter(crypto => crypto.name.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        if (select) {
+            result = result.filter(crypto => crypto.payment.toLowerCase().includes(select.toLowerCase()));
+        }
+
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+};
