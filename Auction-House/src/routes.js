f6ab16bbf5b69df4c052b8e426d9a5d6569cfd01
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-17 14:01:38 +0300

    Add error page to routes

--- Auction-House/src/routes.js
+++ Auction-House/src/routes.js
@@ -5,9 +5,13 @@ const userController = require('./controllers/userController');
 const auctionController = require('./controllers/auctionController');
 
 const { users, auctions } = require('./configs/utils');
+const { inccorectUrl, errorPage } = require('./utils/routes');
 
 router.use(homeController);
 router.use(users, userController);
 router.use(auctions, auctionController);
+router.get(inccorectUrl, (req, res) => {
+    res.render(errorPage);
+});
 
 module.exports = router;
