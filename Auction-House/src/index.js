382bdc94e8c3d89a359ecbbb69c401dd1f77dc79
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-17 10:55:34 +0300

    Add db and connect it

--- Auction-House/src/index.js
+++ Auction-House/src/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const expressConfigs = require('./configs/expressConfigs');
 const handlebarsConfigs = require('./configs/handlebarsConfigs');
+const connectDb = require('./configs/database');
 const routes = require('./routes');
 
 const { PORT } = require('./configs/utils');
 
 const app = express();
+
+connectDb()
+    .then(() => console.log('DB connected successfully'))
+    .catch(err => console.log(`Error: ${err}`));
 
 expressConfigs(app);
 handlebarsConfigs(app);
